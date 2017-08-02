af92b2881f7433bdfa8f22be377906384fa5ecf8
papillon <devopreet.ch@gmail.com>
2017-08-02 12:18:34 +0530

    added skeletal repos

---
+++ src/main/java/qwe/asd/sample1/model/repositories/AuthorRepository.java
@@ -0,0 +1,9 @@
+package qwe.asd.sample1.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import qwe.asd.sample1.model.Author;
+
+@Repository("authorrepository")
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
---
+++ src/main/java/qwe/asd/sample1/model/repositories/BookRepository.java
@@ -0,0 +1,9 @@
+package qwe.asd.sample1.model.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import qwe.asd.sample1.model.Book;
+
+@Repository("bookrepository")
+public interface BookRepository extends CrudRepository<Book,Long> {
+}
